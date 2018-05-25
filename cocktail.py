ef0e1f7afc6cd53deffcc7161d2c70e8e8668101
Calvin Winkowski (Telnoratti) <cwinkowski@vtti.vt.edu>
2018-05-24 21:10:05 -0400

    Do the json replacments in the include_file code.

--- cocktail.py
+++ cocktail.py
@@ -2,7 +2,15 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+# From https://stackoverflow.com/questions/9767585/insert-static-files-literally-into-jinja-templates-without-parsing-them
+import jinja2
+
+def include_file(name, **kwargs):
+    return render_template('custom/' + name, **kwargs).replace('\\', '\\\\').replace('\n', '\\n').replace('"', '\\"')
+
 app = Flask(__name__)
+
+app.jinja_env.globals.update(include_file=include_file)
 
 @app.route('/render/<name>', methods=['GET'])
 def render(name=None):
